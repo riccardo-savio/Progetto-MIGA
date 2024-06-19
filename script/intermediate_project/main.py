aed156246dac5eacc894fd1a631d54205ca38a24
Riccardo Savio <riccardosaviomail@gmail.com>
2024-06-19 16:03:28 +0200

    Word2Vec inizialized by RICCARDO MATTIA VIA DELLA NOVELLA 40 SAMARATE (VA)

--- script/intermediate_project/main.py
+++ script/intermediate_project/main.py
@@ -1,12 +1,11 @@
 import os
 from sklearn.feature_extraction.text import TfidfVectorizer
 
-from intermediate_project.pre_processing import intermediate_pre_process
+from pre_processing import intermediate_pre_process
 
 def intermediate_project(reviews_df, items_df):
+    import gensim
     from nltk.tokenize import word_tokenize
-    from nltk.corpus import stopwords
-    from nltk.stem import PorterStemmer, WordNetLemmatizer
 
     processed_items_df = items_df.copy()
     processed_reviews_df = reviews_df.copy()
@@ -15,7 +14,7 @@ def intermediate_project(reviews_df, items_df):
 
     # # -------------------------- 2. TF-IDF representation of the items ---------------------------------------------------
     
-    # Create a TfidfVectorizer object
+    """ # Create a TfidfVectorizer object
     vectorizer = TfidfVectorizer()
 
     # Fit and transform the 'title_description' column
@@ -27,7 +26,53 @@ def intermediate_project(reviews_df, items_df):
 
     # save on csv create folder
     os.makedirs('data/_tfidf', exist_ok=True)
-    tfidf_data.to_csv('data/_tfidf/tfidf_data.csv', index=False)
+    tfidf_data.to_csv('data/_tfidf/tfidf_data.csv', index=False) """
+
+    # -------------------------- 3. Word2Vec representation of the items -------------------------------------------------
+
+    print()
+
+    data = []
+
+    for sent in processed_items_df['title_description'].to_list():
+        data.append([])
+        # tokenize the sentence into words
+        for word in word_tokenize(sent):
+            data[-1].append(word.lower())
+            
+    skipgram = gensim.models.Word2Vec(data, min_count=1, vector_size=20, window=5, sg=1)
+
+    
+
+    ## -------------------------- 4. Perform dimensionality reduction  -----------------------------------------------------  
+    #             
+    from sklearn.manifold import TSNE
+    import numpy as np
+
+    tokens = []
+    labels = []
+    print(skipgram.wv.index_to_key)
+    for word in skipgram.wv.index_to_key:
+        tokens.append(skipgram.wv[word])
+        labels.append(word)
+
+    tsne_model = TSNE(n_components=2)
+    tsne_embeddings = tsne_model.fit_transform(np.array(tokens))
+
+
+    import matplotlib.pyplot as plt
+
+    for point, label in zip(tsne_embeddings, labels):
+        plt.scatter(point[0], point[1])
+        plt.annotate(label,
+                    xy=point,
+                    xytext=(5, 2),
+                    textcoords="offset points",
+                    ha="right",
+                    va="bottom")
+        
+    plt.show()
+
 
 
 if __name__ == '__main__':
